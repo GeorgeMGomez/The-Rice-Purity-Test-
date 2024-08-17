78a5ada096490f5abeff154bd58a98956b06ca20
Amabel <george.gomez04032@gmail.com>
2024-08-18 02:32:45 +0300

    Added more things to CSS and js file. I also added a file that contains the planning of the test calculations and results.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
-function check(){
-    
+function calculate(){
+    let score = 100;
+
+    if(document.getElementById("1").checked){
+        //Question: Held hands romantically?
+        score = score - 1;
+    }
+
+    if(document.getElementById("2").checked){
+        //Question: Been on a date?
+        score = score - 1;
+    }
+
+    if(document.getElementById("3").checked){
+        //Question: Been in a relationship?
+        score = score - 1;
+    }
+
+    if(document.getElementById("4").checked){
+        //Question: Danced without leaving room for Jesus?
+        score = score - 1;
+    }
+
+    if(document.getElementById("5").checked){
+        //Question: Kissed a non-family member?
+        score = score - 1;
+    }
+
+    playResult(score);
+}
+
+function playResult(score){
+    if (score>=95) {
+        document.body.innerHTML = `<div>
+        <h1 class="totalScore">Total Score: ${score}</h1>
+        <img src="/assets/images/Holy Ending.jpg" alt="Clouds and a cross hovering over them">
+        </div>
+        `;
+    }else if(score>=75){
+        
+    }
 }
