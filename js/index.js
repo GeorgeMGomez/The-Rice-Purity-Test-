960c94e91c563c22d81a925fc6cae50df83c0d69
GeorgeMGomez <george@George>
2024-08-23 17:05:25 +0300

    Fixed addresses

--- js/index.js
+++ js/index.js
@@ -33,42 +33,42 @@ function playResult(score){
     if (score>=95) {
         document.body.innerHTML = `<div>
         <h1 class="totalScore">Total Score: ${score}</h1>
-        <img src="/assets/images/Holy Ending.jpg" alt="Clouds and a cross hovering over them.">
+        <img src="assets/images/Holy Ending.jpg" alt="Clouds and a cross hovering over them.">
         </div>
         `;
-        var audio = new Audio('/assets/sounds/HolyEnding.MP3');
+        var audio = new Audio('assets/sounds/HolyEnding.MP3');
         audio.play();
     }else if(score>=75){
         document.body.innerHTML = `<div>
         <h1 class="totalScore">Total Score: ${score}</h1>
-        <img src="/assets/images/AverageEnding.jpg" alt="An old man smiling with a thumbs up.">
+        <img src="assets/images/AverageEnding.jpg" alt="An old man smiling with a thumbs up.">
         </div>
         `;
-        var audio = new Audio('/assets/sounds/AverageEnding.MP3');
+        var audio = new Audio('assets/sounds/AverageEnding.MP3');
         audio.play();
     }else if(score>=50){
         document.body.innerHTML = `<div>
         <h1 class="totalScore">Total Score: ${score}</h1>
-        <img src="/assets/images/BorderlineMoralEnding.jpg" alt="Gus fring being nervous for the first time.">
+        <img src="assets/images/BorderlineMoralEnding.jpg" alt="Gus fring being nervous for the first time.">
         </div>
         `;
-        var audio = new Audio('/assets/sounds/BorderlineMoralEnding.MP3');
+        var audio = new Audio('assets/sounds/BorderlineMoralEnding.MP3');
         audio.play();
     }else if(score>=25){
         document.body.innerHTML = `<div>
         <h1 class="totalScore">Total Score: ${score}</h1>
-        <img src="/assets/images/SayGoodByeToHeaven.jpg" alt="An old man smiling with a thumbs up.">
+        <img src="assets/images/SayGoodByeToHeaven.jpg" alt="An old man smiling with a thumbs up.">
         </div>
         `;
-        var audio = new Audio('/assets/sounds/SayGoodByeToHeaven.MP3');
+        var audio = new Audio('assets/sounds/SayGoodByeToHeaven.MP3');
         audio.play();
     }else{
         document.body.innerHTML = `<div>
         <h1 class="totalScore"><span style="color:red">Total Score: ${score}</span></h1>
-        <img src="/assets/images/YouNeedToGoToJailEnding.jpg" alt="An old man smiling with a thumbs up.">
+        <img src="assets/images/YouNeedToGoToJailEnding.jpg" alt="An old man smiling with a thumbs up.">
         </div>
         `;
-        var audio = new Audio('/assets/sounds/YouNeedToGoToJailEnding.MP3');
+        var audio = new Audio('assets/sounds/YouNeedToGoToJailEnding.MP3');
         audio.play();
     }
 }
