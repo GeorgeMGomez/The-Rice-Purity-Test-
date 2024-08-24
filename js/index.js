83eec4ac93685553c5d8d0de862243b9de3eb097
GeorgeMGomez <george.gomez04032@gmail.com>
2024-08-24 21:34:49 +0300

    added all options

--- js/index.js
+++ js/index.js
@@ -1,33 +1,509 @@
-function calculate(){
+function calculate() {
     let score = 100;
 
-    if(document.getElementById("1").checked){
-        //Question: Held hands romantically?
+    if (document.getElementById("1").checked) {
+        // Question: Held hands romantically?
         score = score - 1;
     }
 
-    if(document.getElementById("2").checked){
-        //Question: Been on a date?
-        score = score - 25;
+    if (document.getElementById("2").checked) {
+        // Question: Been on a date?
+        score = score - 1;
     }
 
-    if(document.getElementById("3").checked){
-        //Question: Been in a relationship?
-        score = score - 50;
+    if (document.getElementById("3").checked) {
+        // Question: Been kissed on the lips?
+        score = score - 1;
     }
 
-    if(document.getElementById("4").checked){
-        //Question: Danced without leaving room for Jesus?
-        score = score - 75;
+    if (document.getElementById("4").checked) {
+        // Question: Kissed someone on the cheek?
+        score = score - 1;
     }
 
-    if(document.getElementById("5").checked){
-        //Question: Kissed a non-family member?
-        score = score - 100;
+    if (document.getElementById("5").checked) {
+        // Question: Kissed someone of the same sex?
+        score = score - 1;
+    }
+
+    if (document.getElementById("6").checked) {
+        // Question: Kissed someone in public?
+        score = score - 1;
+    }
+
+    if (document.getElementById("7").checked) {
+        // Question: Gone on a date with someone you met online?
+        score = score - 1;
+    }
+
+    if (document.getElementById("8").checked) {
+        // Question: Dated someone for over a year?
+        score = score - 1;
+    }
+
+    if (document.getElementById("9").checked) {
+        // Question: Been engaged?
+        score = score - 1;
+    }
+
+    if (document.getElementById("10").checked) {
+        // Question: Been married?
+        score = score - 1;
+    }
+
+    if (document.getElementById("11").checked) {
+        // Question: Held hands with someone in public?
+        score = score - 1;
+    }
+
+    if (document.getElementById("12").checked) {
+        // Question: Had a long-distance relationship?
+        score = score - 1;
+    }
+
+    if (document.getElementById("13").checked) {
+        // Question: Been on a romantic getaway?
+        score = score - 1;
+    }
+
+    if (document.getElementById("14").checked) {
+        // Question: Lived with a significant other?
+        score = score - 1;
+    }
+
+    if (document.getElementById("15").checked) {
+        // Question: Had a breakup?
+        score = score - 1;
+    }
+
+    if (document.getElementById("16").checked) {
+        // Question: Been dumped?
+        score = score - 1;
+    }
+
+    if (document.getElementById("17").checked) {
+        // Question: Dumped someone?
+        score = score - 1;
+    }
+
+    if (document.getElementById("18").checked) {
+        // Question: Been heartbroken?
+        score = score - 1;
+    }
+
+    if (document.getElementById("19").checked) {
+        // Question: Cried over a breakup?
+        score = score - 1;
+    }
+
+    if (document.getElementById("20").checked) {
+        // Question: Had a crush on someone older?
+        score = score - 1;
+    }
+
+    if (document.getElementById("21").checked) {
+        // Question: Had a crush on a teacher?
+        score = score - 1;
+    }
+
+    if (document.getElementById("22").checked) {
+        // Question: Had a crush on a friend's partner?
+        score = score - 1;
+    }
+
+    if (document.getElementById("23").checked) {
+        // Question: Been in love?
+        score = score - 1;
+    }
+
+    if (document.getElementById("24").checked) {
+        // Question: Been told "I love you"?
+        score = score - 1;
+    }
+
+    if (document.getElementById("25").checked) {
+        // Question: Told someone "I love you"?
+        score = score - 1;
+    }
+
+    if (document.getElementById("26").checked) {
+        // Question: Had a crush on a celebrity?
+        score = score - 1;
+    }
+
+    if (document.getElementById("27").checked) {
+        // Question: Had a crush on a fictional character?
+        score = score - 1;
+    }
+
+    if (document.getElementById("28").checked) {
+        // Question: Had a crush on someone you shouldn't?
+        score = score - 1;
+    }
+
+    if (document.getElementById("29").checked) {
+        // Question: Gone on a double date?
+        score = score - 1;
+    }
+
+    if (document.getElementById("30").checked) {
+        // Question: Gone on a group date?
+        score = score - 1;
+    }
+
+    if (document.getElementById("31").checked) {
+        // Question: Sent a love letter?
+        score = score - 1;
+    }
+
+    if (document.getElementById("32").checked) {
+        // Question: Received a love letter?
+        score = score - 1;
+    }
+
+    if (document.getElementById("33").checked) {
+        // Question: Written a poem or song for someone?
+        score = score - 1;
+    }
+
+    if (document.getElementById("34").checked) {
+        // Question: Had a secret relationship?
+        score = score - 1;
+    }
+
+    if (document.getElementById("35").checked) {
+        // Question: Been in a long-term relationship?
+        score = score - 1;
+    }
+
+    if (document.getElementById("36").checked) {
+        // Question: Been on a road trip with a partner?
+        score = score - 1;
+    }
+
+    if (document.getElementById("37").checked) {
+        // Question: Traveled internationally with a partner?
+        score = score - 1;
+    }
+
+    if (document.getElementById("38").checked) {
+        // Question: Cried over a love story?
+        score = score - 1;
+    }
+
+    if (document.getElementById("39").checked) {
+        // Question: Had a crush on a stranger?
+        score = score - 1;
+    }
+
+    if (document.getElementById("40").checked) {
+        // Question: Been serenaded?
+        score = score - 1;
+    }
+
+    if (document.getElementById("41").checked) {
+        // Question: Serenaded someone?
+        score = score - 1;
+    }
+
+    if (document.getElementById("42").checked) {
+        // Question: Been in a love triangle?
+        score = score - 1;
+    }
+
+    if (document.getElementById("43").checked) {
+        // Question: Had a relationship end badly?
+        score = score - 1;
+    }
+
+    if (document.getElementById("44").checked) {
+        // Question: Been on a blind date?
+        score = score - 1;
+    }
+
+    if (document.getElementById("45").checked) {
+        // Question: Been catfished?
+        score = score - 1;
+    }
+
+    if (document.getElementById("46").checked) {
+        // Question: Dated more than one person at the same time?
+        score = score - 1;
+    }
+
+    if (document.getElementById("47").checked) {
+        // Question: Ghosted someone?
+        score = score - 1;
+    }
+
+    if (document.getElementById("48").checked) {
+        // Question: Been ghosted?
+        score = score - 1;
+    }
+
+    if (document.getElementById("49").checked) {
+        // Question: Been stood up on a date?
+        score = score - 1;
+    }
+
+    if (document.getElementById("50").checked) {
+        // Question: Had a one-night stand?
+        score = score - 1;
+    }
+
+    if (document.getElementById("51").checked) {
+        // Question: Hooked up with someone you met online?
+        score = score - 1;
+    }
+
+    if (document.getElementById("52").checked) {
+        // Question: Hooked up with a friend?
+        score = score - 1;
+    }
+
+    if (document.getElementById("53").checked) {
+        // Question: Hooked up with an ex?
+        score = score - 1;
+    }
+
+    if (document.getElementById("54").checked) {
+        // Question: Had a friends-with-benefits arrangement?
+        score = score - 1;
+    }
+
+    if (document.getElementById("55").checked) {
+        // Question: Had a summer fling?
+        score = score - 1;
+    }
+
+    if (document.getElementById("56").checked) {
+        // Question: Had a rebound relationship?
+        score = score - 1;
+    }
+
+    if (document.getElementById("57").checked) {
+        // Question: Been in an open relationship?
+        score = score - 1;
+    }
+
+    if (document.getElementById("58").checked) {
+        // Question: Been in a polyamorous relationship?
+        score = score - 1;
+    }
+
+    if (document.getElementById("59").checked) {
+        // Question: Experimented with your sexuality?
+        score = score - 1;
+    }
+
+    if (document.getElementById("60").checked) {
+        // Question: Been to a pride parade?
+        score = score - 1;
+    }
+
+    if (document.getElementById("61").checked) {
+        // Question: Been to a music festival?
+        score = score - 1;
+    }
+
+    if (document.getElementById("62").checked) {
+        // Question: Been to a rave?
+        score = score - 1;
+    }
+
+    if (document.getElementById("63").checked) {
+        // Question: Gone clubbing?
+        score = score - 1;
+    }
+
+    if (document.getElementById("64").checked) {
+        // Question: Danced with someone at a party?
+        score = score - 1;
+    }
+
+    if (document.getElementById("65").checked) {
+        // Question: Had a crush on a friend's sibling?
+        score = score - 1;
+    }
+
+    if (document.getElementById("66").checked) {
+        // Question: Had a crush on a sibling's friend?
+        score = score - 1;
+    }
+
+    if (document.getElementById("67").checked) {
+        // Question: Been caught sneaking out?
+        score = score - 1;
+    }
+
+    if (document.getElementById("68").checked) {
+        // Question: Snuck out of your house?
+        score = score - 1;
+    }
+
+    if (document.getElementById("69").checked) {
+        // Question: Lied to your parents?
+        score = score - 1;
+    }
+
+    if (document.getElementById("70").checked) {
+        // Question: Gotten grounded?
+        score = score - 1;
+    }
+
+    if (document.getElementById("71").checked) {
+        // Question: Been caught lying?
+        score = score - 1;
+    }
+
+    if (document.getElementById("72").checked) {
+        // Question: Stolen something?
+        score = score - 1;
+    }
+
+    if (document.getElementById("73").checked) {
+        // Question: Shoplifted?
+        score = score - 1;
+    }
+
+    if (document.getElementById("74").checked) {
+        // Question: Cheated on a test?
+        score = score - 1;
+    }
+
+    if (document.getElementById("75").checked) {
+        // Question: Played a prank on someone?
+        score = score - 1;
+    }
+
+    if (document.getElementById("76").checked) {
+        // Question: Pulled an all-nighter?
+        score = score - 1;
+    }
+
+    if (document.getElementById("77").checked) {
+        // Question: Been in a physical fight?
+        score = score - 1;
+    }
+
+    if (document.getElementById("78").checked) {
+        // Question: Been arrested?
+        score = score - 1;
+    }
+
+    if (document.getElementById("79").checked) {
+        // Question: Broken a bone?
+        score = score - 1;
+    }
+
+    if (document.getElementById("80").checked) {
+        // Question: Gotten stitches?
+        score = score - 1;
+    }
+
+    if (document.getElementById("81").checked) {
+        // Question: Gotten a speeding ticket?
+        score = score - 1;
+    }
+
+    if (document.getElementById("82").checked) {
+        // Question: Been in a car accident?
+        score = score - 1;
+    }
+
+    if (document.getElementById("83").checked) {
+        // Question: Gotten into a fender bender?
+        score = score - 1;
+    }
+
+    if (document.getElementById("84").checked) {
+        // Question: Gotten a parking ticket?
+        score = score - 1;
+    }
+
+    if (document.getElementById("85").checked) {
+        // Question: Gotten lost while driving?
+        score = score - 1;
+    }
+
+    if (document.getElementById("86").checked) {
+        // Question: Been in an ambulance?
+        score = score - 1;
+    }
+
+    if (document.getElementById("87").checked) {
+        // Question: Been on a stretcher?
+        score = score - 1;
+    }
+
+    if (document.getElementById("88").checked) {
+        // Question: Gotten into a bar fight?
+        score = score - 1;
+    }
+
+    if (document.getElementById("89").checked) {
+        // Question: Been in a police chase?
+        score = score - 1;
+    }
+
+    if (document.getElementById("90").checked) {
+        // Question: Driven under the influence?
+        score = score - 1;
+    }
+
+    if (document.getElementById("91").checked) {
+        // Question: Had your license suspended?
+        score = score - 1;
+    }
+
+    if (document.getElementById("92").checked) {
+        // Question: Pretended to be sick?
+        score = score - 1;
+    }
+
+    if (document.getElementById("93").checked) {
+        // Question: Pretended to be someone else?
+        score = score - 1;
+    }
+
+    if (document.getElementById("94").checked) {
+        // Question: Been hospitalized?
+        score = score - 1;
+    }
+
+    if (document.getElementById("95").checked) {
+        // Question: Gotten a tattoo?
+        score = score - 1;
+    }
+
+    if (document.getElementById("96").checked) {
+        // Question: Gotten a piercing?
+        score = score - 1;
+    }
+
+    if (document.getElementById("97").checked) {
+        // Question: Gone skinny dipping?
+        score = score - 1;
+    }
+
+    if (document.getElementById("98").checked) {
+        // Question: Been to a nude beach?
+        score = score - 1;
+    }
+
+    if (document.getElementById("99").checked) {
+        // Question: Done something you regret?
+        score = score - 1;
+    }
+
+    if (document.getElementById("100").checked) {
+        // Question: Done something you wish you could take back?
+        score = score - 1;
     }
 
     playResult(score);
 }
+
 
 function playResult(score){
     if (score>=95) {
